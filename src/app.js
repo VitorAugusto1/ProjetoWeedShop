4c20301b025c442489147f6e9091d69bfa242c59
Vitor <vitornm9@gmail.com>
2026-02-21 19:28:40 -0300

    First commit

---
+++ src/app.js
@@ -0,0 +1,44 @@
+const express = require('express');
+const { createClient } = require('@supabase/supabase-js');
+require('dotenv').config();
+
+const port = 3000;
+const users = [];
+
+const supabase = createClient(process.env.SUPABASE_URL, process.env.SUPABASE_KEY);
+
+
+const app = express();
+app.use(express.json());
+
+app.post("/register", async (req, res) => {
+    const { email, password } = req.body;
+
+    const { data, error } = await supabase.auth.signUp({
+        email,
+        password
+    });
+
+    if (error) return res.status(400).json({ error: error.message });
+
+    res.json({ message: 'Usuário registrado com sucesso', user: data.user });
+});
+
+app.post('/login', async (req, res) => {
+    const { email, password } = req.body;
+
+    const { data, error } = await supabase.auth.signInWithPassword({
+        email,
+        password
+    });
+
+    if (error) {
+        return res.status(400).json({ message: "Credenciais inválidas" });
+    }
+
+    return res.json({ message: 'Login realizado com sucesso'});
+});
+
+app.listen(port, ()=> {
+    console.log(`Servidor rodando em http://localhost:${port}`);
+});
